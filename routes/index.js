532090d52227ebe335886ac7823cd6e7d464b23d
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-21 23:19:01 -0500

    displaying json data instead of view pages

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var data = [1,2,3,4,5,6,7,8,9,10]
-  res.render('index', { title: 'Express',data });
+  res.json(data)
 });
 
 module.exports = router;
