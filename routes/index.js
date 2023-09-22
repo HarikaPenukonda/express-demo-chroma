5daa4bd84da2e1559cbc10257e96f11afdd8296c
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-21 23:08:01 -0500

    jsp

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var data = [1,2,3,4,5,6,7,8,9,10]
+  res.render('index', { title: 'Express',data });
 });
 
 module.exports = router;
