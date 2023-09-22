146ae72c17d86d497b9397f360ca06065a118502
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-21 23:21:58 -0500

    json data

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var data = [1,2,3,4,5,6,7,8,9,10]
+  let data = {}
+  data.arr = [1,2,3,4,5,6,7,8,9,10]
+  data.users = [
+    {name : 'andy',age:22},
+    {name : 'john',age:45},
+    {name : 'bob', age:50}
+  ]
   res.json(data)
 });
 
