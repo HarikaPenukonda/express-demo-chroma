b76a3f15b1a58681e41aca9c21bb6e6671d3fcc0
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-23 19:13:38 -0500

    /top-deal-users - get method

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var {topDealUsers}= require("../data");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({message : 'respond with a resource'});
+router.get('/top-deal-users', function(req, res, next) {
+  //res.json({message : 'respond with a resource'});
+  res.json({topDealUsers})
 });
 
 module.exports = router;
